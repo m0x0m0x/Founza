50a0fe9c485dc3e2497815185c39cc947bc701c3
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-09 12:22:05 +0000

    🐌 :f1

---
+++ w3/env.fish
@@ -0,0 +1,12 @@
+#!/bin/fish
+
+# #### API ####
+
+set MXE 2JEANQYC4C9S6PKDFWNGVT2UER24T32D2M
+
+# #### HTTPS Provider #### 
+
+set https://polygon-mumbai.g.alchemy.com/v2/fZOoxA6iUz8s3JquDiu4VatGa53OIVNv 
+
+# ### Wallets #### 
+
