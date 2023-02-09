43f1ca36f2462bf71423a871468aa92a1752b976
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-09 12:32:53 +0000

    🐌 :f1

--- w3/env.fish
+++ w3/env.fish
@@ -6,7 +6,15 @@ set MXE 2JEANQYC4C9S6PKDFWNGVT2UER24T32D2M
 
 # #### HTTPS Provider #### 
 
-set https://polygon-mumbai.g.alchemy.com/v2/fZOoxA6iUz8s3JquDiu4VatGa53OIVNv 
+set ALE https://polygon-mumbai.g.alchemy.com/v2/fZOoxA6iUz8s3JquDiu4VatGa53OIVNv 
 
 # ### Wallets #### 
 
+# ❯ cast wallet vanity --starts-with 69
+# Starting to generate vanity address...
+# Successfully found vanity address in 0 seconds.
+# Address: 0x6937478e83357428e0C22F60605790f4157cC4a6
+# Private Key: 0xce043f3e348e936418a49a35af5e70443a96ffb33551c9fb0822754d7614bb74
+
+set FX1 0x6937478e83357428e0C22F60605790f4157cC4a6
+set FX1K 0xce043f3e348e936418a49a35af5e70443a96ffb33551c9fb0822754d7614bb74
