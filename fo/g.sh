799a92480e68b3087e8cafe5b7475708170cf7d8
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-09 13:36:29 +0000

    🐒:r1

--- fo/g.sh
+++ fo/g.sh
@@ -3,7 +3,7 @@ echo "==================="
 echo ""
 echo "git add -all && git commit -m <addurshit>&& git push"
 git add --all 
-git commit -m "🐌 :f1"
+git commit -m "🐒:r1"
 git push --progress
 echo ""
 echo "=========PUSH DONE========="
