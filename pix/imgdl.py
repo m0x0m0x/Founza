d995763f8cacb887029cc303562ee06fd1fa19b5
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-09 13:58:19 +0000

    🐒:r1

--- pix/imgdl.py
+++ pix/imgdl.py
@@ -4,6 +4,6 @@ Convert WEBP image to gif - Works perfectly fine
 source of candy = https://www.sex.com/search/gifs?query=pawg+ass
 '''
 from PIL import Image
-im = Image.open('this.webp')
+im = Image.open('t.webp')
 im.info.pop('background', None)
-im.save('that.gif', 'gif', save_all=True)
+im.save('to.gif', 'gif', save_all=True)
